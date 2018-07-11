78c9f3a0794e1c3c369d1b4505f2cfa58da39197
RyanOliverSotto <Ryan.Sotto@Outlook.com>
2018-07-10 19:35:18 -0500

    Sequelize and Models created

---
+++ models/kiosk.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+    var Kiosk = sequelize.define("Kiosk", {
+      // Giving the Author model a name of type STRING
+      kiosk: DataTypes.STRING
+    });
+    Kiosk.associate = function(models) {
+      // Associating Kiosk with Books
+      Kiosk.hasMany(models.Book);
+    };
+    return Kiosk;
+  };
+  
