1ee63a1670bb19c0e9cc1cb937229811f9d5cf0d
ryanoliversotto <ryan.sotto@outlook.com>
2018-07-10 23:21:15 -0500

    Added seeds SQL for distribution to the team

---
+++ db/seeds.sql
@@ -0,0 +1,41 @@
+## Populate the kiosk tbl
+
+USE cubex_db;
+INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('HOWARD');
+INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('JARVIS');
+INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('MORSE');
+INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('LUNT');
+INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('LOYOLA');
+INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('GRANVILLE');
+
+## Populate the genres table
+INSERT INTO genres (`genre`) VALUES ('Arts & Photography');
+INSERT INTO genres (`genre`) VALUES ('Biographies & Memoirs');
+INSERT INTO genres (`genre`) VALUES ('Business & Money');
+INSERT INTO genres (`genre`) VALUES ('Calendars');
+INSERT INTO genres (`genre`) VALUES ('Childrens Books');
+INSERT INTO genres (`genre`) VALUES ('Comics & Graphic Novels');
+INSERT INTO genres (`genre`) VALUES ('Computers & Technology');
+INSERT INTO genres (`genre`) VALUES ('Cookbooks');
+INSERT INTO genres (`genre`) VALUES ('Hobbies & Home');
+INSERT INTO genres (`genre`) VALUES ('Christian Books & Bibles');
+INSERT INTO genres (`genre`) VALUES ('Engineering & Transportation');
+INSERT INTO genres (`genre`) VALUES ('Health');
+INSERT INTO genres (`genre`) VALUES ('History');
+INSERT INTO genres (`genre`) VALUES ('Humor & Entertainment');
+INSERT INTO genres (`genre`) VALUES ('Law  ');
+INSERT INTO genres (`genre`) VALUES ('Literature & Fiction');
+INSERT INTO genres (`genre`) VALUES ('Medical Books');
+INSERT INTO genres (`genre`) VALUES ('Mystery, Thriller & Suspense');
+INSERT INTO genres (`genre`) VALUES ('Parenting & Relationships');
+INSERT INTO genres (`genre`) VALUES ('Politics & Social Sciences');
+INSERT INTO genres (`genre`) VALUES ('Reference');
+INSERT INTO genres (`genre`) VALUES ('Religion & Spirituality');
+INSERT INTO genres (`genre`) VALUES ('Romance');
+INSERT INTO genres (`genre`) VALUES ('Science & Math');
+INSERT INTO genres (`genre`) VALUES ('Science Fiction & Fantasy');
+INSERT INTO genres (`genre`) VALUES ('Self-Help');
+INSERT INTO genres (`genre`) VALUES ('Sports & Outdoors');
+INSERT INTO genres (`genre`) VALUES ('Teen & Young Adult   ');
+INSERT INTO genres (`genre`) VALUES ('Test Preparation ');
+INSERT INTO genres (`genre`) VALUES ('Travel');
