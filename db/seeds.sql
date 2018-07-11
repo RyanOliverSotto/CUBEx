6f7e7a466ef2d898d9a11d4a54c753136a4e8238
ryanoliversotto <ryan.sotto@outlook.com>
2018-07-11 17:42:53 -0500

    Scrubbed the dataset. Able to populate MYSQL

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 ## Populate the kiosk tbl
-
+## RUN THESE TWO FIRST
 USE cubex_db;
 INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('HUB');
 INSERT INTO `cubex_db`.`kiosks` (`kiosk`) VALUES ('HOWARD');
@@ -40,3 +40,40 @@ INSERT INTO genres (`genre`) VALUES ('Sports & Outdoors');
 INSERT INTO genres (`genre`) VALUES ('Teen & Young Adult   ');
 INSERT INTO genres (`genre`) VALUES ('Test Preparation ');
 INSERT INTO genres (`genre`) VALUES ('Travel');
+
+
+## There is an offset with the big dataset and the GENRE ID. Table alteration is needed if your local 
+## copy of MYSQL starts genre at 1. Run this AFTER RUNNING FIRST TWO PARAGRAPHS
+## USE IMPORT WIZARD TO capture
+UPDATE `cubex_db`.`genres` SET `id`='0' WHERE `id`='1';
+UPDATE `cubex_db`.`genres` SET `id`='1' WHERE `id`='2';
+UPDATE `cubex_db`.`genres` SET `id`='2' WHERE `id`='3';
+UPDATE `cubex_db`.`genres` SET `id`='3' WHERE `id`='4';
+UPDATE `cubex_db`.`genres` SET `id`='4' WHERE `id`='5';
+UPDATE `cubex_db`.`genres` SET `id`='5' WHERE `id`='6';
+UPDATE `cubex_db`.`genres` SET `id`='6' WHERE `id`='7';
+UPDATE `cubex_db`.`genres` SET `id`='7' WHERE `id`='8';
+UPDATE `cubex_db`.`genres` SET `id`='8' WHERE `id`='9';
+UPDATE `cubex_db`.`genres` SET `id`='9' WHERE `id`='10';
+UPDATE `cubex_db`.`genres` SET `id`='10' WHERE `id`='11';
+UPDATE `cubex_db`.`genres` SET `id`='11' WHERE `id`='12';
+UPDATE `cubex_db`.`genres` SET `id`='12' WHERE `id`='13';
+UPDATE `cubex_db`.`genres` SET `id`='13' WHERE `id`='14';
+UPDATE `cubex_db`.`genres` SET `id`='14' WHERE `id`='15';
+UPDATE `cubex_db`.`genres` SET `id`='15' WHERE `id`='16';
+UPDATE `cubex_db`.`genres` SET `id`='16' WHERE `id`='17';
+UPDATE `cubex_db`.`genres` SET `id`='17' WHERE `id`='18';
+UPDATE `cubex_db`.`genres` SET `id`='18' WHERE `id`='19';
+UPDATE `cubex_db`.`genres` SET `id`='19' WHERE `id`='20';
+UPDATE `cubex_db`.`genres` SET `id`='20' WHERE `id`='21';
+UPDATE `cubex_db`.`genres` SET `id`='21' WHERE `id`='22';
+UPDATE `cubex_db`.`genres` SET `id`='22' WHERE `id`='23';
+UPDATE `cubex_db`.`genres` SET `id`='23' WHERE `id`='24';
+UPDATE `cubex_db`.`genres` SET `id`='24' WHERE `id`='25';
+UPDATE `cubex_db`.`genres` SET `id`='25' WHERE `id`='26';
+UPDATE `cubex_db`.`genres` SET `id`='26' WHERE `id`='27';
+UPDATE `cubex_db`.`genres` SET `id`='27' WHERE `id`='28';
+UPDATE `cubex_db`.`genres` SET `id`='28' WHERE `id`='29';
+UPDATE `cubex_db`.`genres` SET `id`='29' WHERE `id`='30';
+
+
--- models/books.js
+++ models/books.js
@@ -32,7 +32,7 @@ module.exports = function (sequelize, DataTypes) {
         //Each book will have a foreign key with GENRE ID
         Book.belongsTo(models.Genre, {
             foreignKey: {
-                allowNull: true
+                allowNull: false
             }
         });
     };
@@ -40,7 +40,7 @@ module.exports = function (sequelize, DataTypes) {
         //Each book will have a foreign key with KIOSK ID
         Book.belongsTo(models.Kiosk, {
             foreignKey: {
-                allowNull: true
+                allowNull: false
             }
         });
     };
