487e2fc55d3dd820df8cacdc5b172e95e27858a5
RyanOliverSotto <Ryan.Sotto@Outlook.com>
2018-07-10 20:43:57 -0500

    Made a change to the MODELS

--- models/kiosk.js
+++ models/kiosk.js
@@ -2,7 +2,11 @@ module.exports = function(sequelize, DataTypes) {
     var Kiosk = sequelize.define("Kiosk", {
       // Giving the Author model a name of type STRING
       kiosk: DataTypes.STRING
-    });
+    },
+    {
+        timestamps: false
+    }
+);
     Kiosk.associate = function(models) {
       // Associating Kiosk with Books
       Kiosk.hasMany(models.Book);
