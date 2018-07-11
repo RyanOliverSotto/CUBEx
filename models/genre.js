dcf43e8df039dac1cbdfc2afc7d5713e5b3a23c5
Val Gabriel <gabriel.valryan@hotmail.com>
2018-07-10 20:48:00 -0500

    something

--- models/genre.js
+++ models/genre.js
@@ -2,7 +2,9 @@ module.exports = function(sequelize, DataTypes) {
     var Genre = sequelize.define("Genre", {
       // Giving the Author model a name of type STRING
       genre: DataTypes.STRING
-    });
+    },
+    {timestamps: false}
+);
   
     Genre.associate = function(models) {
       // Associating Genre with Books
